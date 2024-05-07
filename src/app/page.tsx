5b16cda01d77a07b22317ba5d5ef17bb7d7c93d1
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 13:54:19 +0200

    Use prisma to fetch data
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import { currentUser } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+import { db } from "~/server/db";
 
 export default async function HomePage() {
   const user = await currentUser();
+
+  if (!user) {
+    return notFound();
+  }
+
+  const todos = await db.todo.findMany({
+    where: {
+      authorId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-[#56a360] to-[#323adb] text-white">
