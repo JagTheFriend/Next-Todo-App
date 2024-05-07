87ab08afa6f6e03eea872e3442ea1d3eeb34323b
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 12:24:17 +0200

    Updated file
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ export default async function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-[#56a360] to-[#323adb] text-white">
-      {!user && (
-        <>
-          <h1 className="cursor-pointer text-2xl transition-all hover:underline">
-            This is a demo Todo App built with Next.js App Router
-          </h1>
-        </>
-      )}
+      <>
+        <h1 className="cursor-pointer text-2xl transition-all hover:underline">
+          This is a demo Todo App built with Next.js App Router
+        </h1>
+      </>
     </main>
   );
 }
