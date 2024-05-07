306df7e84bd7373dc28577765be2579b046b4e99
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 15:17:42 +0200

    Display loading state
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/component/InputField.tsx
+++ src/component/InputField.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { useFormStatus } from "react-dom";
 import addTodo from "~/actions";
 
 export default function DisplayInputField() {
@@ -17,13 +18,25 @@ export default function DisplayInputField() {
         className="input input-accent w-full max-w-lg"
         onInput={(e) => setInputContent(e.currentTarget.value.trim())}
       />
-      <button
-        type="submit"
-        className="btn btn-primary btn-outline"
-        disabled={inputContent.length === 0}
-      >
-        Add TODO
-      </button>
+      <SubmitButton inputContent={inputContent} />
     </form>
+  );
+}
+
+function SubmitButton({ inputContent }: { inputContent: string }) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type="submit"
+      className="btn btn-primary btn-outline"
+      disabled={inputContent.length === 0 || pending}
+    >
+      {pending ? (
+        <span className="loading loading-dots loading-md" />
+      ) : (
+        "Add TODO"
+      )}
+    </button>
   );
 }
