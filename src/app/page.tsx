157025ab963c97b7f0a6151aecade6f60eb9c4a1
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 16:57:00 +0200

    Use form to send request
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { notFound } from "next/navigation";
+import { Bounce, toast } from "react-toastify";
+import { deleteTodo } from "~/actions";
 import DisplayInputField from "~/component/InputField";
 import { db } from "~/server/db";
 
@@ -40,9 +42,41 @@ async function DisplayTodos() {
         <li key={todo.id} className="items-center border-b-2 p-2">
           <div className="flex flex-row items-center gap-4">
             <p className="truncate text-2xl">{todo.content}</p>
-            <button type="button" className="btn btn-outline btn-error">
-              Delete
-            </button>
+            <form
+              action={async (formData) => {
+                "use server";
+                const data = await deleteTodo(todo.id);
+                if (data?.error) {
+                  return toast.error(data.message, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                  });
+                }
+
+                toast.success(`Deleted Todo '${todo.content}'`, {
+                  position: "top-center",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                  transition: Bounce,
+                });
+              }}
+            >
+              <button type="submit" className="btn btn-outline btn-error">
+                Delete
+              </button>
+            </form>
           </div>
         </li>
       ))}
