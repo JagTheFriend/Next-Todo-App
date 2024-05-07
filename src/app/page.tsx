a161870f4b7ff34463af2d8b969b1af4ded3fed2
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 14:12:28 +0200

    Display username
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,27 @@ import { currentUser } from "@clerk/nextjs/server";
 import { notFound } from "next/navigation";
 import { db } from "~/server/db";
 
+async function DisplayUsername() {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div className="mt-4 flex items-center justify-center">
+      <h1 className="cursor-default truncate font-medium hover:underline sm:text-3xl md:text-3xl lg:text-5xl">
+        Welcome{" "}
+        {user.username ??
+          user.firstName ??
+          user.lastName ??
+          user.fullName ??
+          "User"}
+        !
+      </h1>
+    </div>
+  );
+}
+
 export default async function HomePage() {
   const user = await currentUser();
 
@@ -19,12 +40,10 @@ export default async function HomePage() {
   });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-[#56a360] to-[#323adb] text-white">
-      <>
-        <h1 className="cursor-pointer text-2xl transition-all hover:underline">
-          This is a demo Todo App built with Next.js App Router
-        </h1>
-      </>
+    <main>
+      <section>
+        <DisplayUsername />
+      </section>
     </main>
   );
 }
