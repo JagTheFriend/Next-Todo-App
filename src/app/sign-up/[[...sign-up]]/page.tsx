f106578e5f8cae7f8bc75eb18e32f973b5bb140e
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 12:14:37 +0200

    Follow docs
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-[#56a360] to-[#323adb] text-white">
+      <h1 className="cursor-pointer text-2xl transition-all hover:underline">
+        This is a demo Todo App built with Next.js App Router
+      </h1>
+      <SignUp routing="hash" />
+    </main>
+  );
+}
