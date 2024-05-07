ac5ced16ad68a36feac74194d3a278e2d19045ed
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 15:04:58 +0200

    Created simple server action
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { db } from "./server/db";
+
+export default async function addTodo(formData: FormData) {
+  const content = (formData.get("content") ?? "").toString();
+  const { userId } = auth();
+
+  if (!content) {
+    return { error: true, message: "Please enter a todo name" };
+  }
+
+  if (!userId) {
+    return { error: true, message: "Please sign in" };
+  }
+
+  try {
+    await db.todo.create({
+      data: {
+        authorId: userId,
+        content,
+      },
+    });
+  } catch (error) {
+    return { error: true, message: "An Error Occurred" };
+  }
+  revalidatePath("/");
+}
