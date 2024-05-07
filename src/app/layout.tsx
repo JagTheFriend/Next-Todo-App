b652434f71f39ed019ee4ff7a1897749b4dbdabe
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-07 11:07:41 +0200

    Added clerk keys
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "~/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
@@ -19,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
