684218f6b6a40432994a6b75ea661e683556bf1f
JagTheFriend <JagTheFriend12@gmail.com>
2024-05-09 18:40:24 +0200

    Fixed type
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
 
-const createPrismaClient = () =>
+type CreatePrismaClient = () => PrismaClient;
+
+const createPrismaClient: CreatePrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: ReturnType<CreatePrismaClient> | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
